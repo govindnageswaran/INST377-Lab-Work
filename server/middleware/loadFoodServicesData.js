afc2dfaefafa8aa14398c596d700d311b1f43a2c
govindnageswaran <govindnageswaran222@gmail.com>
2022-11-17 17:45:50 -0500

    Update loadFoodServicesData.js

--- server/middleware/loadFoodServicesData.js
+++ server/middleware/loadFoodServicesData.js
@@ -16,7 +16,7 @@ export async function loadFoodServiceData(req, res, next) {
     const json = await data.json(); // the data isn't json until we access it using dot notation
     const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
     console.log('Results in foodServiceData middleware', json.length); // let's check that something's there before we return it
-    req.foodServiceData = json; // and let's _attach_ the data to our request object here
+    req.foodServiceData = reply; // and let's _attach_ the data to our request object here
     next();
   } catch (err) {
     // and let's handle any errors by closing the request with a message
